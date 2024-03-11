2771514703b9d02a8a2891cc641cc33ae9124a9e
Alex H <agalexaandro16@gmial.com>
2024-03-11 19:01:39 +0100

    create pot

--- main.py
+++ main.py
@@ -6,21 +6,9 @@ def sumArray(array):
     result += i
   return result
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-chips = 100
-
 def deal(array):
   array.append(random.choice(cards))
   return array
-
-def payOut(houseScore, playerScore, chips, bet):
-  if playerScore > 21 or playerScore < houseScore:
-    print('you lost :(')
-  else:
-    chips += bet*2
-    print('you win! :D')
-  return chips
 
 def housePlay():
   houseCards = []
@@ -33,5 +21,19 @@ def housePlay():
     score = sumArray(houseCards)
     print(houseCards, score)
   return score
+
+def payOut(houseScore, playerScore, chips, bet):
+  if playerScore > 21 or playerScore < houseScore:
+    print('you lost :(')
+  else:
+    chips += bet*2
+    print('you win! :D')
+  return chips
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+chips = 100
+
+pot = 0
 
 print(housePlay())
